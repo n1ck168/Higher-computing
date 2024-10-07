89568426ce9d0e0b6a6105c28f24d3ad67dcd336
Nick Wilkinson <N.Wilkinson@albynschool.org>
2024-10-07 11:13:54 +0100

    filehandling task 2

---
+++ filehandling.py
@@ -0,0 +1,6 @@
+#-------------Main Program-----------------
+#open file
+with open("newfile.txt","w") as writefile:
+    writefile.write("Nick Wilkinson, blue/purple, july")
+
+# Run the program and see what it does, then adapt the code so that the program writes your full name, favourite colour and birth month to the file instead.
