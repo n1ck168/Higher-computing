6529ed05a88a3425c4acf984643c7a4338589fb2
Nick Wilkinson <N.Wilkinson@albynschool.org>
2024-11-01 08:53:56 +0000

    securepasswordmaker2.py

--- secure password maker.py
+++ secure password maker.py
@@ -3,11 +3,27 @@
 
 #The program should repeatedly ask the user to enter a secure password until they successfully do so. Please include helpful error messages.
 
-OriginalPas = input("Please enter a secure passoword")
-while ord(OriginalPas[0]) < 65:
-    print("error, the first letter of your password needs to be a capital")
-    OriginalPas = input("Please enter a secure password")
-while str(OriginalPas(-1)) not == "#" or not == "$" or not == "%"
-    print("error, your password must end in one of the following characters: $, #, %")
-    OriginalPas = input("Please enter a secure password")
+def isCapital(letter):
+    if ord(letter) > 90:
+        print("error, the first letter of your password needs to be a capital")
+        return False
+    else:
+        return True
 
+def lastSymbol(lastletter):
+    if not lastletter  == "#" and not lastletter == "$" and not lastletter == "%":
+        print("error, your password must end in one of the following characters: $, #, %")
+        return False
+    else:
+        return True
+
+OriginalPas = input("Please enter a secure passoword")
+print(ord(OriginalPas[0]))
+lastletter =  (OriginalPas[len(OriginalPas)-1:len(OriginalPas)])
+
+while not isCapital(OriginalPas[0]) or not lastSymbol(lastletter):
+    OriginalPas = input("Please enter a secure passoword")
+    print(ord(OriginalPas[0]))
+    lastletter =  (OriginalPas[len(OriginalPas)-1:len(OriginalPas)])
+
+print("All ok")
