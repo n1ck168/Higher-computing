bc90a6e70d253a3160bfacc7107129c6363dd765
Nick Wilkinson <N.Wilkinson@albynschool.org>
2025-01-31 10:01:23 +0000

    sdfsdf

--- UFO sdd recap p1.py
+++ UFO sdd recap p1.py
@@ -1,13 +1,13 @@
 # RECAP - UFO
 # Original task by Mr Neil, adapted by Mr Simpson
-
+count = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,]
 import csv
 thisDate = []
 country = []
 location = []
 shape = []
 description = []
-filePath = 'Software Design and Development/RECAP - UFO/'
+filePath = ''
 
 # -------------------------------------------------- DO NOT ALTER -----
 def importFile():
@@ -22,4 +22,45 @@ def importFile():
     return thisDate, country, location, shape, description
 # -------------------------------------------------- DO NOT ALTER -----
 
-thisDate, country, location, shape, description = importFile()
+
+def countPerCountry(countryarray, specifiedcountry):
+    numSightings = 0 
+    for i in range (0,len(countryarray)):
+        if countryarray[i] == specifiedcountry:
+            numSightings = numSightings + 1
+    return numSightings
+
+
+
+def displaySightings(specifiedcountry, numSightings):
+    print("There are", numSightings, "sightings that took place in", specifiedcountry)
+
+
+
+def countyearsightings(count, thisDate):
+    countposition = 0
+    for i in range (0,len(thisDate)):
+        currentyear = thisDate[i]6:11]
+        nextyear = thisDate[i+1][6:11]
+        if currentyear == nextyear:
+            print(currentyear)
+            print(countposition)
+            print(count[countposition])
+            count[countposition] = count[countposition] + 1
+        else: 
+            countposition = countposition + 1
+            print(currentyear, count[countposition -1])
+
+
+
+
+
+
+
+
+# main program
+thisDate, country, location, shape, description = importFile() # 1. ImportFile IN: none OUT: thisDate, country, location, shape, description
+specifiedcountry = input("Please enter a country")
+numSightings = countPerCountry(country, specifiedcountry) # 2. countPerCountry IN: country[], specifiedCountry OUT: numSightings
+displaySightings(specifiedcountry, numSightings) #  3. displaySightings IN: specifiedCountry, numSightings OUT: none
+countyearsightings(count, thisDate)
